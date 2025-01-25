2f8c5c599f2f31b6bce90d0b661859faeb909b0f
Victor Campos <victorjvc2020@gmail.com>
2025-01-25 12:16:22 -0300

    Add template string e mapeamento de colunas

--- main.py
+++ main.py
@@ -37,38 +37,61 @@ def get_mensagem_de_texto(texto_path: str) -> Template:
         mensagem = arquivo_txt.read().strip()
     return Template(mensagem)
     
+def get_mapeamento_colunas(planilha) -> dict[str, int]:
+    """ Mapeia os nomes das colunas para seus índices.
+    :param planilha: Worksheet do openpyxl
+    :return: Dicionário com os nomes das colunas como chaves e os índices como valores
+    """
+    mapeamento = {}
+    for idx, cell in enumerate(planilha[1], start=1):  # Primeira linha como cabeçalho
+        if cell.value:  # Ignorar colunas sem nome
+            mapeamento[cell.value] = idx
+    return mapeamento
 
-# Main TODO: Ceritificar que user tá logado no wpp web
+
 if __name__ == '__main__':
-    # Seleciona o arquivo de texto para a mensagem
-    texto_path: str = selecionar_arquivo('.txt')
-    mensagem: str = get_mensagem_de_texto(texto_path=texto_path)
-
-    print(f'{mensagem=}\n')
+    ### planilha, mapeamento_colunas e template_mensagem ###
     
-    planilha_path: str = selecionar_arquivo('.xlsx')
+    # Seleciona arquivo de texto, planilha e mapeia índices colunas.
+    try:
+        texto_path: str = selecionar_arquivo('.txt')
+        template_mensagem: Template = get_mensagem_de_texto(texto_path)
+        
+        workbook_path = selecionar_arquivo('.xlsx')
+        workbook = openpyxl.load_workbook(workbook_path)    
+
+        # Mostrar nomes das paginas: 
+        nomes_paginas = workbook.sheetnames 
+        
+        planilha = workbook[nomes_paginas[0]] 
+        mapeamento_colunas: dict = get_mapeamento_colunas(planilha)
+        INDEX_NOME: int = mapeamento_colunas.get('nome') - 1
+        INDEX_TELEFONE: int = mapeamento_colunas.get('telefone') - 1
+        # INDEX_EMAIL: int = mapeamento_colunas.get('email') - 1
+    except FileNotFoundError as e:
+        print(f"Erro: {e}")
+    except Exception as e:
+        print(f"Erro inesperado: {e}")
     
-    workbook = openpyxl.load_workbook(planilha_path)
-    planilha_clientes = workbook[workbook.sheetnames[0]] # 
-    # TODO:  deixar a seleção por str ['nome_col']
-    for linha in planilha_clientes.iter_rows(min_row=2):
-        NOME = linha[0].value
-        TELEFONE = linha[1].value
-
-        if not NOME or not TELEFONE:  # Ignora linhas incompletas
-            continue
-
+    ### -------------------------- Envio de mensagens  --------------------- ###
+    for linha in planilha.iter_rows(min_row=2):
+        NOME: str = linha[INDEX_NOME].value.split(' ')[0]
+        TELEFONE: str = str(linha[INDEX_TELEFONE].value)
+        
+        # Ignora linhas incompletas
+        if not NOME or not TELEFONE:  
+            continue  
+        mensagem = template_mensagem.substitute(nome=NOME, telefone=TELEFONE) 
         try:
-            # Abre o WhatsApp Web com o link formatado
             webbrowser.open(f'https://web.whatsapp.com/send?phone={TELEFONE}&text={quote(mensagem)}')
-            time.sleep(10)  # Aguarda o envio da mensagem
-            pyautogui.hotkey('enter')  # Fecha a aba do navegador
+            time.sleep(20)  
+            pyautogui.hotkey('enter')  
             time.sleep(5)  # Aguarda o envio da mensagem
             pyautogui.hotkey('ctrl', 'w')  # Fecha a aba do navegador
+            time.sleep(3)
         except Exception as e:
             print(f'Erro ao enviar mensagem para {NOME}: {e}')
-            # Salva o erro em um arquivo
+            # TODO:  Salvar o erro em um arquivo
             with open('erros.csv', 'a', newline='', encoding='utf-8') as arquivo:
                 arquivo.write(f'{NOME},{TELEFONE}{os.linesep}')
-
     print("Envio de mensagens concluído!")
